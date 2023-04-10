24088112d9b0029d88046c9989c94d2e0c12443f
jamie-cloud99 <j1999j88@gmail.com>
2023-04-10 21:29:04 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add .
+git commit -m 'deploy'
+
+# if you are deploying to https://jamie-cloud99.github.io
+# git push -f git@github.com:jamie-cloud99/jamie-cloud99.github.io.git main
+
+# if you are deploying to https://jamie-cloud99.github.io/vite-deploy
+git push -f git@github.com:jamie-cloud99/vite-deploy.git main:gh-pages
+
+cd -
