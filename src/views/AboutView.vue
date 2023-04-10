4346986cb1428286bab22dcbaab7d0a479fbc4c6
jamie-cloud99 <j1999j88@gmail.com>
2023-04-10 17:06:52 +0800

    建立vite環境

---
+++ src/views/AboutView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: {}
+    }
+  },
+  mounted () {
+    // 引入環境變數
+    // console.log(import.meta.env.VITE_PATH)
+    const url = import.meta.env.VITE_PATH
+    console.log(import.meta.env.VITE_TEXT)
+    this.$http.get(url)
+      .then(res => {
+        this.data = res.data.results[0]
+        console.log(this.data)
+      })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
